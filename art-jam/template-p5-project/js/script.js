23af84f113af665c9c571bf7e9675185ec066d7c
rabiamariam14-arch <rabia.mariam14@gmail.com>
2025-09-30 20:05:03 -0400

    background and windows

--- art-jam/template-p5-project/js/script.js
+++ art-jam/template-p5-project/js/script.js
@@ -1,8 +1,8 @@
 /**
- * Title of Project
- * Author Name
+ * DAFLA
+ * MARIAM RABIA
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * A REPRESANTATION OF MY CHILDHOOD ROOM WITH A NEW INTERPRETATION
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
  */
 
@@ -11,7 +11,13 @@
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
+let flowerX;
+let flowerY;
 function setup() {
+    createCanvas(800, 600);
+    flowerX = width / 2
+    flowerY = -20
+    ellipseMode(CENTER)
 
 }
 
@@ -20,5 +26,15 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(245);
+
+    if (mouseX > width / 2) {
+        fill(255,230,180)
+    } else {
+        fill(200,220,250)
+    }
+    rect(100,100, 100, 120)
+
 
 }
+
